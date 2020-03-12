1f03c897959ecda2a5ec1bfca3a3c7a49815bdb6
youdozi <eschoi@systemk.kr>
2020-03-12 12:59:57 +0900

    eslint, kakaoMap 설정 변경

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,28 @@
       </v-col>
 
       <v-col class="mb-4">
+        <vue-daum-map
+                :appKey="appKey"
+                :center.sync="center"
+                :level.sync="level"
+                :mapTypeId="mapTypeId"
+                :libraries="libraries"
+                @load="onLoad"
+                @center_changed="onMapEvent('center_changed', $event)"
+                @zoom_start="onMapEvent('zoom_start', $event)"
+                @zoom_changed="onMapEvent('zoom_changed', $event)"
+                @bounds_changed="onMapEvent('bounds_changed', $event)"
+                @click="onMapEvent('click', $event)"
+                @dblclick="onMapEvent('dblclick', $event)"
+                @rightclick="onMapEvent('rightclick', $event)"
+                @mousemove="onMapEvent('mousemove', $event)"
+                @dragstart="onMapEvent('dragstart', $event)"
+                @drag="onMapEvent('drag', $event)"
+                @dragend="onMapEvent('dragend', $event)"
+                @idle="onMapEvent('idle', $event)"
+                @tilesloaded="onMapEvent('tilesloaded', $event)"
+                @maptypeid_changed="onMapEvent('maptypeid_changed', $event)"
+                style="width:100%;height:400px;"/>
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
@@ -92,8 +114,12 @@
 </template>
 
 <script>
+  import VueDaumMap from 'vue-daum-map';
+  import config from '../config';
+
   export default {
     name: 'HelloWorld',
+    components: {VueDaumMap},
 
     data: () => ({
       ecosystem: [
@@ -146,6 +172,27 @@
           href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
         },
       ],
+      appKey: config.appKey, // 테스트용 appkey
+      center: {lat:33.450701, lng:126.570667}, // 지도의 중심 좌표
+      level: 3, // 지도의 레벨(확대, 축소 정도),
+      mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
+      libraries: [], // 추가로 불러올 라이브러리
+      mapObject: null // 지도 객체. 지도가 로드되면 할당됨.
     }),
+
+    methods: {
+      // 지도가 로드 완료되면 load 이벤트 발생
+      onLoad (map) {
+        // 지도의 현재 영역을 얻어옵니다
+        var bounds = map.getBounds();
+        // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
+        var boundsStr = bounds.toString();
+        console.log('Daum Map Loaded', boundsStr);
+        this.mapObject = map;
+      },
+      onMapEvent (event, params) {
+        console.log(`Daum Map Event(${event})`, params);
+      }
+    }
   }
 </script>
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+    appKey: "0a938dc798602ab0bd7060e6693a8e29"
+}
